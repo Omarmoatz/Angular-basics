8dc4841394fe6c89ad850e7f5f9ee01a5f84f9c7
Omarmoatz <omar.raouf.002@gmail.com>
2025-04-16 10:42:43 +0200

    used component methods

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -8,8 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  constructor(){
+    console.log("constructor >>>>>>>>>>>>>>>>");
+
+  }
+
   ngOnInit(): void {
     console.log("in on init>>>>>>>");
+  }
+  
+  ngAfterContentInit(): void {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    console.log("ngAfterContentInit >>>>>>>>>>>>>>>>");
+
+  }
+
+  ngAfterContentChecked(): void {
+    //Called after every check of the component's or directive's content.
+    console.log("ngAfterContentChecked >>>>>>>>>>>>>>>>");
+  }
+
+  ngAfterViewChecked(): void {
+    //Called after every check of the component's view. Applies to components only.
+    console.log("ngAfterViewChecked >>>>>>>>>>>>>>>>");
+
+    
+  }
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    console.log("ngAfterViewInit >>>>>>>>>>>>>>>>");
+    
+  }
+
+  ngDoCheck(): void {
+    //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+    console.log("ngDoCheck >>>>>>>>>>>>>>>>");
+    
+  }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    console.log("ngOnChanges >>>>>>>>>>>>>>>>");
+    
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    console.log("ngOnDestroy >>>>>>>>>>>>>>>>");
+    
   }
 
 }
