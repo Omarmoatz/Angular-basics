c994230ed759caf4ee6e93ce32f23f9286b2c463
Omarmoatz <omar.raouf.002@gmail.com>
2025-05-01 16:15:16 +0300

    xgfd

--- src/app/phone-validator.directive.ts
+++ src/app/phone-validator.directive.ts
@@ -16,7 +16,24 @@ export class PhoneValidatorDirective implements Validator {
   constructor() { }
 
   validate(control: AbstractControl): ValidationErrors | null {
-    throw new Error('Method not implemented.');
+    let pattern = /[1-9]{1}[0-9]{9}/;
+
+    console.log(pattern);
+    
+    console.log("control------------------------------------");
+    console.log("control", control);
+    console.log("control.value.length", control.value.length);
+    console.log("control.value.match(pattern)", control.value.match(pattern));
+    
+
+    if(control.value && control.value.length < 10 && control.value.match(pattern)){
+      
+      console.log("wrong xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+      return {"InvalidPhoneNum": false};
+    }
+
+    return null;
+
   }
   
   // registerOnValidatorChange?(fn: () => void): void {
--- src/app/register-user/register-user.component.ts
+++ src/app/register-user/register-user.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { User } from '../../models/user.model';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { PhoneValidatorDirective } from '../phone-validator.directive';
 
 @Component({
   selector: 'app-register-user',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, PhoneValidatorDirective],
   templateUrl: './register-user.component.html',
   styleUrl: './register-user.component.css'
 })
